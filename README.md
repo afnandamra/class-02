c80c22a6a2d99f94b4f9fd2cd88044e0cc6fe8ab
afnandamra <damra.afnan@gmail.com>
2021-01-05 18:01:43 +0200

    lab 3 update

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# LAB - 02
+# LAB - 03
 
 ## About Me Project
 
@@ -9,12 +9,8 @@ This webpage is made for code 201 course, it contains information about me and a
 ### Links and Resources
 * [submission Link](https://github.com/afnandamra/class-02)
 * [Puplished Site](https://afnandamra.github.io/class-02/)
-* [Setting Up A New 201 Code Project](https://codefellows.github.io/code-201-guide/curriculum/class-02/project_setup)
 
 ### Reflections and Comments
-* This was a very fun lab to do, I hope all the next labs will be the same or better!
-* I learned a bunch of new stuff:
-    * How to add a link to an image
-    * How to add a video
-    * How to modify a list
-* Figuering out CSS commands was the hardest part :sweat_smile:
+* This lab was harder than usual. It was fun though.
+* I learned nested loops, and arrays.
+* Now we can do a little bit more complicated wesites, can't wait to learn more and more functions!
--- js/app.js
+++ js/app.js
@@ -1,58 +1,94 @@
-'use strict'
+'use strict';
 
-var username=prompt('What\'s your name?');
-
+var username = prompt('What\'s your name?');
 greeting();
-
-var question1=prompt('Am I a girl?');
+var score = 0;
+//1
+var question1 = prompt('Am I a girl?');
 correctyes(question1);
-var question2=prompt('Did I study computer science?');
+//2
+var question2 = prompt('Did I study computer science?');
 correctno(question2);
-var question3=prompt('Was I born in the 90\'s?');
+//3
+var question3 = prompt('Was I born in the 90\'s?');
 correctyes(question3);
-var question4=prompt('Do you like my work?');
+//4
+var question4 = prompt('Do you like my work?');
 correctyes(question4);
-var question5=prompt('Do I hate TV?');
+//5
+var question5 = prompt('Do I hate TV?');
 correctno(question5);
+//6
+for (var i = 1; i < 5; i++) {
+    var question6 = prompt('How many feilds did I have an internship in?')
+    if (Number(question6) === 4) {
+        alert('That is correct!');
+        score++;
+        i = 5;
+    } else if (Number(question6) < 4) {
+        alert('Too low!');
+    } else if (Number(question6) > 4) {
+        alert('Too high!');
+    } else {
+        alert('Please insert a number!');
+    }
+}
+//7
+var BFfood = ['sandwiches', 'pancake', 'eggs', 'yogurt bowls', 'cereal'];
+for (var i = 6; i >= 1; i--){
+    var question7 = prompt('What is my favorite breakfast? '+i+' attempts left!');
+    for (var index=0; index < BFfood.length; index++){
+        if (question7.toLowerCase() === BFfood[index]){
+            // console.log(BFfood[index]);
+            alert('Correct! '+question7+ ' is one of my favorite breakfasts!');
+            index = BFfood.length;
+            i = 0;
+        }
+    }
+}
 
-alert('Thank you for visiting my webpage '+username+'!');
+alert('My favorite breakfast food are ' + BFfood);
+alert('Congratulations ' + username + '! You got ' + score + ' out of 7. Good Job!');
+alert('Thank you for visiting my webpage ' + username + '!');
 
 
 // functions declaration
-function greeting(){
-        var today= new Date();
-        var t = today.getHours();
-        var greeting;
-        
-        if (t > 18){
-            greeting='Good Evening, ';
-        } else if (t > 12){
-            greeting='Good Afternoon, ';
-        } else if (t > 6){
-            greeting='Good Morning, ';
-        } else {
-            greeting='Welcome, ';
-        }
-    
-    return document.write('<h2>'+greeting+username+'!'+'</h2>');
-}
-
-function correctyes(q){
-    if(q.toLowerCase() === 'yes' || q.toUpperCase()=== 'Y'){
-//        console.log('Correct!');
+function correctyes(q) {
+    if (q.toLowerCase() === 'yes' || q.toUpperCase() === 'Y') {
+        //        console.log('Correct!');
         alert('That is correct! Good for you.');
+        score++;
     } else {
-//        console.log('Wrong!');
+        //        console.log('Wrong!');
         alert('Wrong!! Better luck next time.');
     }
 }
 
-function correctno(q){
-    if(q.toLowerCase() === 'no' || q.toUpperCase()=== 'N'){
-//        console.log('Correct!');
+function correctno(q) {
+    if (q.toLowerCase() === 'no' || q.toUpperCase() === 'N') {
+        //        console.log('Correct!');
         alert('That is correct! Good for you.');
+        score++;
     } else {
-//        console.log('Wrong!');
+        //        console.log('Wrong!');
         alert('Wrong!! Better luck next time.');
     }
+}
+
+function greeting() {
+    var today = new Date();
+    var t = today.getHours();
+    var greeting;
+
+    if (t > 18) {
+        greeting = 'Good Evening, ';
+    } else if (t > 12) {
+        greeting = 'Good Afternoon, ';
+    } else if (t > 6) {
+        greeting = 'Good Morning, ';
+    } else {
+        greeting = 'Welcome, ';
+    }
+
+    return document.write('<h2>' + greeting + username + '!' + '</h2>');
 }
