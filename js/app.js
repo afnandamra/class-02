e2b9b2123abeab1517765f48ac148b59114afc3e
afnandamra <damra.afnan@gmail.com>
2021-01-06 10:35:46 +0200

    feedback update

--- js/app.js
+++ js/app.js
@@ -20,33 +20,52 @@ var question5 = prompt('Do I hate TV?');
 correctno(question5);
 //6
 for (var i = 1; i < 5; i++) {
-    var question6 = prompt('How many feilds did I have an internship in?')
-    if (Number(question6) === 4) {
+    var question6 = prompt('How old am I?')
+    if (Number(question6) === 25) {
         alert('That is correct!');
         score++;
-        i = 5;
-    } else if (Number(question6) < 4) {
+        break;
+        // i = 5; it is not good to miss with the counter!
+    } else if (Number(question6) < 25) {
         alert('Too low!');
-    } else if (Number(question6) > 4) {
+    } else if (Number(question6) > 25) {
         alert('Too high!');
     } else {
         alert('Please insert a number!');
     }
 }
 //7
-var BFfood = ['sandwiches', 'pancake', 'eggs', 'yogurt bowls', 'cereal'];
-for (var i = 6; i >= 1; i--) {
+var BFfood = ['sandwiches', 'pancakes', 'eggs', 'yogurt bowls', 'cereal'];
+var i = 6, correct = false;
+while (i >= 1 && !correct) {
     var question7 = prompt('What is my favorite breakfast? ' + i + ' attempts left!');
     for (var index = 0; index < BFfood.length; index++) {
         if (question7.toLowerCase() === BFfood[index]) {
             // console.log(BFfood[index]);
             alert('Correct! ' + question7 + ' is one of my favorite breakfasts!');
             score++;
-            index = BFfood.length;
-            i = 0;
+            correct = !correct;
+            break;
+            // index = BFfood.length;
+            // i = 0;
         }
     }
+    i--;
 }
+// for (var i = 6; i >= 1; i--) {
+//     var question7 = prompt('What is my favorite breakfast? ' + i + ' attempts left!');
+//     for (var index = 0; index < BFfood.length; index++) {
+//         if (question7.toLowerCase() === BFfood[index]) {
+//             // console.log(BFfood[index]);
+//             alert('Correct! ' + question7 + ' is one of my favorite breakfasts!');
+//             score++;
+//             correct = !correct;
+//             break;
+//             // index = BFfood.length;
+//             // i = 0;
+//         }
+//     }
+// }
 
 alert('My favorite breakfast food are ' + BFfood);
 alert('Congratulations ' + username + '! You got ' + score + ' out of 7. Good Job!');
@@ -55,14 +74,28 @@ alert('Thank you for visiting my webpage ' + username + '!');
 
 // functions declaration
 function correctyes(q) {
-    if (q.toLowerCase() === 'yes' || q.toUpperCase() === 'Y') {
-        //        console.log('Correct!');
-        alert('That is correct! Good for you.');
-        score++;
-    } else {
-        //        console.log('Wrong!');
-        alert('Wrong!! Better luck next time.');
+    switch (q.toLowerCase()){
+        case 'yes':
+        case 'y':
+            alert('That is correct! Good for you.');
+            score++;
+            break;
+        case 'no':
+        case 'n':
+            alert('Wrong!! Better luck next time.');
+            break;
+        default:
+            alert('It is a yes/no question!');
+            break;
     }
+    // if (q.toLowerCase() === 'yes' || q.toUpperCase() === 'Y') {
+    //     //        console.log('Correct!');
+    //     alert('That is correct! Good for you.');
+    //     score++;
+    // } else {
+    //     //        console.log('Wrong!');
+    //     alert('Wrong!! Better luck next time.');
+    // }
 }
 
 function correctno(q) {
